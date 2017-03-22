d53aa330e95b968665b599596edf550064e33402
Marc Neudert <marc.neudert@gmail.com>
2017-03-22 19:56:23 +0100

    bootstraps travis docker acceptance tests

---
+++ test/acceptance/test.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+# change cwd to current script dir
+cd "${0%/*}" || exit 127
+
+# setup global configuration
+export BUNDLE_GEMFILE='../../Gemfile'
+export VAGRANT_I_KNOW_WHAT_IM_DOING_PLEASE_BE_QUIET='true'
+
+# utility functions
+setup() {
+  echo 'Installing dependencies...'
+  bundle install > /dev/null
+
+  echo 'Starting containers...'
+  bundle exec vagrant up > /dev/null
+
+  trap teardown EXIT
+}
+
+teardown() {
+  echo 'Destroying containers...'
+  bundle exec vagrant destroy -f > /dev/null
+}
+
+trim() {
+  sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' <<< "${1}"
+}
+
+# run tests
+setup
+
+echo 'Running tests...'
+
+result=$(trim "$(bundle exec vagrant ssh primary -c 'ls -la /test/project_a')")
+
+[[ "${result}" == *'file_a'* ]] || {
+  echo 'Mountpoint invalid or missing...'
+  echo "Got result: '${result}'"
+  exit 1
+}
+
+result=$(trim "$(bundle exec vagrant ssh secondary -c 'ls -la /test/project_a')")
+
+[[ "${result}" == *'file_a'* ]] || {
+  echo 'Mountpoint invalid or missing...'
+  echo "Got result: '${result}'"
+  exit 1
+}
+
+echo 'Success!'
